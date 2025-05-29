a3d96dd24d0a9d2fd2a64c71bde6e278d1680c10
DatWithTheWorld <nguyenducdat592004@gmail.com>
2025-05-29 23:19:12 +0700

    udpate filed

--- backend/routes/favorites.py
+++ backend/routes/favorites.py
@@ -1,15 +1,29 @@
 from flask import Blueprint, jsonify, request
 from app import db
 from models.favorite import Favorite
+from models.movie import Movie
 
 bp = Blueprint('favorites', __name__)
 
 @bp.route('/favorites/<int:user_id>', methods=['GET'])
 def get_favorites(user_id):
     favorites = Favorite.query.filter_by(user_id=user_id).all()
-    return jsonify([{
-        'movie_id': f.movie_id
-    } for f in favorites])
+    favorite_movies = []
+    for f in favorites:
+        movie = Movie.query.get(f.movie_id)
+        if movie:
+            favorite_movies.append({
+                'id': movie.id,
+                'title': movie.title,
+                'genres': movie.genres,
+                'year': movie.year,
+                'description': movie.description,
+                'trailer_url': movie.trailer_url,
+                'trailer_local': movie.trailer_local,
+                'director': movie.director,
+                'actors': movie.actors
+            })
+    return jsonify(favorite_movies)
 
 @bp.route('/favorites', methods=['POST'])
 def add_favorite():
--- backend/routes/history.py
+++ backend/routes/history.py
@@ -1,16 +1,32 @@
 from flask import Blueprint, jsonify, request
 from app import db
 from models.watch_history import WatchHistory
+from models.movie import Movie
 
 bp = Blueprint('history', __name__)
 
 @bp.route('/history/<int:user_id>', methods=['GET'])
 def get_history(user_id):
     history = WatchHistory.query.filter_by(user_id=user_id).all()
-    return jsonify([{
-        'movie_id': h.movie_id,
-        'watched_at': h.watched_at.isoformat()
-    } for h in history])
+    result = []
+
+    for h in history:
+        movie = Movie.query.get(h.movie_id)
+        if movie:
+            result.append({
+                'id': movie.id,
+                'title': movie.title,
+                'genres': movie.genres,
+                'year': movie.year,
+                'description': movie.description,
+                'trailer_url': movie.trailer_url,
+                'trailer_local': movie.trailer_local,
+                'director': movie.director,
+                'actors': movie.actors,
+                'watched_at': h.watched_at.isoformat()
+            })
+
+    return jsonify(result)
 
 @bp.route('/history', methods=['POST'])
 def add_history():
